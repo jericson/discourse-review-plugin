7d872b07540bde98e888ff07c2f891baf9094161
Jon Ericson <jon@jlericson.com>
2024-07-31 19:39:39 -0700

    Remove routes.rb

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Review::Engine.routes.draw do
-  #get "/examples" => "examples#index"
-  # define routes here
-end
-
-Discourse::Application.routes.draw { mount ::Review::Engine, at: "discourse-review-plugin" }
